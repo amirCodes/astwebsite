5a163eac18c54ed37297ca6f3a9a04375576aa4f
amirCodes <amirdanish126@gmail.com>
2024-06-19 07:20:53 +0800

    added top navbar and grid section ..

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,33 @@
-import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
+import HeroSection from "@/components/HeroSection";
+import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import Grid from "@/components/Grid";
 
+const navItems = [
+  {
+    name: "Home",
+    link: "/",
+    icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "About",
+    link: "/about",
+    icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "Contact",
+    link: "/contact",
+    icon: <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+];
 export default function Home() {
   return (
     <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
         <HeroSection />
+        <Grid />
         {/* <FloatingNav navItems={navItems} />
         <Grid />
         <RecentProjects />
