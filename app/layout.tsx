891e7822ce93dc3d4b0c5724a1eba373454691ae
amirCodes <amirdanish126@gmail.com>
2024-07-22 02:47:19 +0800

    added TT pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,45 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "AmirCodes",
-  description: "Discover specialized expertise in crafting impactful landing pages tailored for advertising purposes and many more. We offer a range of solutions designed to enhance your online presence and drive engagement effectively.",
-  keywords: ["software", "LandingPage", "Landing page", "World Map","coding","website", "amircodes"],
+  description:
+    "Discover specialized expertise in crafting impactful landing pages tailored for advertising purposes and many more. We offer a range of solutions designed to enhance your online presence and drive engagement effectively.",
+  keywords: [
+    "software",
+    "LandingPage",
+    "Landing page",
+    "World Map",
+    "coding",
+    "website",
+    "amircodes"
+  ],
   openGraph: {
-    images: './logo.png',
+    images: "./logo.png"
   }
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-      <script>
+    <html lang="en" suppressContentEditableWarning>
+      <Script strategy="lazyOnload" id="CQEKVHRC77UEVQNL6FM0">
         {`
         !function (w, d, t) {
   w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
 var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")
 ;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
-
-
-  ttq.load('CQEKVHRC77UEVQNL6FM0');
-  ttq.page();
-}(window, document, 'ttq');
+ttq.load('CQEKVHRC77UEVQNL6FM0');
+  ttq.page(); }(window, document, 'ttq');
         `}
-
-</script>
-
-        {children}</body>
+      </Script>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
